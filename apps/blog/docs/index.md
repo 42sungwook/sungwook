ed1ccc3b801d1fd1a7bf30b7acffbee379acb6ba
Sungwook Kim <silenc3@naver.com>
2025-05-28 14:16:43 +0900

    chore: rspress nx setting

--- apps/blog/docs/index.md
+++ apps/blog/docs/index.md
@@ -1,1 +1,12 @@
-# Hello world
+---
+pageType: home
+hero:
+  name: '성욱 블로그'
+  actions:
+    - theme: brand
+      text: '블로그 보기'
+      link: /blog
+    - theme: alt
+      text: '소개'
+      link: /about
+---
--- apps/blog/rspress.config.ts
+++ apps/blog/rspress.config.ts
@@ -1,5 +1,21 @@
 import { defineConfig } from 'rspress/config';
+import { join } from 'path';
 
 export default defineConfig({
-  root: 'docs',
+  root: join(__dirname, 'docs'),
+  title: 'My Blog',
+  description: 'My Blog Description',
+  base: '/',
+  builderConfig: {
+    output: {
+      distPath: {
+        root: join(__dirname, 'dist'),
+      },
+    },
+  },
+  themeConfig: {
+    socialLinks: [
+      // 소셜 링크 설정
+    ],
+  },
 });
