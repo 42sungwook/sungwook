d06859f587158d9118169b7240b1482b1b2cea97
Sungwook Kim <silenc3@naver.com>
2025-06-01 18:52:59 +0900

    fix: delete standalone config

--- apps/home/next.config.mjs
+++ apps/home/next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'standalone',
-  trailingSlash: false,
-  experimental: {
-    serverActions: true,
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
